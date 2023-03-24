0645ac9dc315f52cb6147a4236d2d8b70a2270b3
David Yrigoyen <dyrigoyen@tyr.com>
2023-03-24 08:54:14 -0700

    sunset and sunrise api implementation

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
 import requests
+import datetime as dt
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
+
+MY_LAT = 33.787914
+MY_LNG = -117.853104
+
+# ------------------------------------------------
+# ISS NOW API
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+#
+# data = response.json()
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+# iss_position = (longitude, latitude)
+#
+# print(iss_position)
+
+# ------------------------------------------------
+parameters = {
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
-
 data = response.json()
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-iss_position = (longitude, latitude)
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+print(sunrise)
+print(sunset)
 
-print(iss_position)
-
+now = dt.datetime.now()
+print(now.hour)
